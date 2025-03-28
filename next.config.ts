a5b575c90dd2b1039317cd76110e4a483b479804
1111 <togtosatsolmn@gmail.com>
2025-03-28 11:08:25 +0800

    design review

--- next.config.ts
+++ next.config.ts
@@ -1,62 +1,48 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   // reactStrictMode: true,
-//   experimental: {
-//     optimizeCss: true,
-//     // nextScriptWorkers: true,
-//     middlewarePrefetch: 'flexible',
-//   },
-//   sitemapSize: 5000,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//         port: "",
-//         pathname: "/**",
-//       },
-//     ],
-//     domains: ["example.com", "another-domain.com","www.shutterstock.com", "images.contentstack.io"],
-//     formats: ["image/webp", "image/avif"],
-//     minimumCacheTTL: 60, 
-//     dangerouslyAllowSVG: true,
-//     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", 
-//   },
-//   compress: true,
-//   compiler: {
-//     emotion: true,
-//   },
-//   transpilePackages: ['@chakra-ui/react', '@chakra-ui/next-js', '@emotion/react'],
-
-//   async headers() {
-//     return [
-//       {
-//         source: "/api/auth/session",
-//         headers: [
-//           {
-//             key: "Cache-Control",
-//             value: "no-store, must-revalidate, max-age=0",
-//           },
-//         ],
-//       },
-//     ];
-//   },
-
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Remove or fix the sitemapSize property
-  eslint: {
-    // This is the correct place for ignoreDuringBuilds
-    ignoreDuringBuilds: true
-  },
+const nextConfig: NextConfig = {
+  // reactStrictMode: true,
   experimental: {
-    optimizeCss: true
-  }
+    optimizeCss: true,
+    // nextScriptWorkers: true,
+    middlewarePrefetch: 'flexible',
+  },
+  sitemapSize: 5000,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: ["example.com", "another-domain.com","www.shutterstock.com", "images.contentstack.io"],
+    formats: ["image/webp", "image/avif"],
+    minimumCacheTTL: 60, 
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", 
+  },
+  compress: true,
+  compiler: {
+    emotion: true,
+  },
+  transpilePackages: ['@chakra-ui/react', '@chakra-ui/next-js', '@emotion/react'],
+
+  async headers() {
+    return [
+      {
+        source: "/api/auth/session",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
+
 };
 
-module.exports = nextConfig;
+export default nextConfig;
